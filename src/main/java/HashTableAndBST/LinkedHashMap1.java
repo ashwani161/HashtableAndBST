e637bb254de1d404ec87a9451a4527cba4306431
ashwani161 <ashwanin65@gmail.com>
2021-04-09 07:22:27 +0530

    Added:Remove the word from the Paragraph

---
+++ src/main/java/HashTableAndBST/LinkedHashMap1.java
@@ -0,0 +1,50 @@
+package HashTableAndBST;
+
+public class LinkedHashMap1<K, V> {
+	private LinkedlistMap<K, V> linkedList;
+
+	public LinkedHashMap1() {
+		this.linkedList = new LinkedlistMap<>();
+	}
+
+	public V get(K key) {
+		MapNode<K, V> mapNode = this.linkedList.search(key);
+		return (mapNode == null) ? null : mapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		MapNode<K, V> mapNode = this.linkedList.search(key);
+		if (mapNode == null) {
+			mapNode = new MapNode<>(key, value);
+			this.linkedList.append(mapNode);
+		} else {
+			mapNode.setValue(value);
+		}
+	}
+	
+	public void remove(K key) {
+		MapNode<K, V> mapNode = this.linkedList.search(key);
+		if (mapNode == null) {
+			System.out.println("Element not found");
+		} else {
+			MapNode<K, V> temp = mapNode.getNext();
+			mapNode.setKey(temp.getKey());
+			mapNode.setValue(temp.getValue());
+			mapNode.setNext(temp.getNext());
+		}
+	}
+
+	public int getCount(K key) {
+		MapNode<K, V> mapNode = this.linkedList.search(key);
+		return (int) ((mapNode == null) ? 0 : mapNode.getValue());
+	}
+
+	@Override
+	public String toString() {
+		return "LinkedHashMap{" + linkedList + '}';
+	}
+
+
+
+	
+}
--- src/main/java/HashTableAndBST/LinkedHashMapTest.java
+++ src/main/java/HashTableAndBST/LinkedHashMapTest.java
@@ -6,20 +6,46 @@ import org.junit.Test;
 public class LinkedHashMapTest {
 	@Test
 	public void givenSentence_WhenWordAreAddedToList_ShouldReturnParanoidFrequency() {
-		String sentence = "Paranoids are not paranoid because they are paranoid but "+
-	                      "because they keep putting themselves deliberately into "+
-				           "paranoid avoidable situations";
-		LinkedHashMap<String, Integer> linkedHashMap= new LinkedHashMap<>();
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations";
+		LinkedHashMap1<String, Integer> linkedHashMap1 = new LinkedHashMap1<>();
 		String[] words = sentence.toLowerCase().split(" ");
 		for (String word : words) {
-			Integer value = linkedHashMap.get(word);
-			if (value == null) value = 1;
-			else value = value + 1;
-			linkedHashMap.add(word,value);
-	}
-		System.out.println(linkedHashMap);
-		int frequency = linkedHashMap.get("paranoid");
+			Integer value = linkedHashMap1.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			linkedHashMap1.add(word, value);
+		}
+		int frequency = linkedHashMap1.get("paranoid");
+		System.out.println(linkedHashMap1);
 		Assert.assertEquals(3, frequency);
 
-}
+	}
+
+
+@Test
+	public void givenSentence_WhenWordAreRemoveToList_ShouldReturnavoidableFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations";
+		LinkedHashMap1<String, Integer> linkedHashMap1 = new LinkedHashMap1<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = linkedHashMap1.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			linkedHashMap1.add(word, value);
+		}
+		linkedHashMap1.remove("avoidable");
+		System.out.println(linkedHashMap1);
+		int frequency = linkedHashMap1.getCount("avoidable");
+		Assert.assertEquals(0, frequency);
+
+	}
+
+
+
 }
